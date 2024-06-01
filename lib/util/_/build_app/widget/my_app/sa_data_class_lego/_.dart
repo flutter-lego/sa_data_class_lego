11050a4fec81c7567fcd3292f80bd6341bb24e33
june lee <melodysdreamj@gmail.com>
2024-06-01 21:01:52 +0900

    Initial commit

---
+++ lib/util/_/build_app/widget/my_app/sa_data_class_lego/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverMyApp()
+// Widget Function() coverSaDataClassLego(Widget Function() materialAppBuilder) {
+//   return () => materialAppBuilder();
+// }
